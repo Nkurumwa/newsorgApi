3c5b9eba5f267f9b77b6f62c54ec3b4585fde1fb
munenelewis <munene2379@gmail.com>
2018-01-30 08:12:30 +0300

    initalizing the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,6 @@
 from flask import Flask
-from .config import devConfig
-from flask_bootstrap import Bootstrap
-from config import config_options
 
-bootstap = Bootstrap()
+#initializing application 
 
-def create_app(config_options):
-    app = Flask(__name__)
-    #creating the app configurations
-        app.config.from_object(config_options[config_name])
+from app import views
 
-
-    #initializing bootstrap
-    bootstap.init_app(app)
-
-    return app
