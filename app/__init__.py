4cc4ebcbbd601b6b10b2febe8d2d6c7ea99a8436
munenelewis <munene2379@gmail.com>
2018-01-30 12:57:39 +0300

    initialized flask extentions

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,7 @@ app = Flask(__name__,instance_relative_config=True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+#initializing flask extentions
+bootsrap = Bootstrap(app)
 from app import views
 
